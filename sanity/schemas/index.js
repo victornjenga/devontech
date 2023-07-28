05b28b4ef62a03c13e8d8ccfd514baa6a1f0d50c
Victor Njenga <84647506+victornjenga@users.noreply.github.com>
2023-07-28 14:05:58 +0300

    yes

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,5 +1,1 @@
-import sites from './sites'
-import user from './user'
-import postedBy from './postedBy'
-import comment from './comment'
-export const schemaTypes = [sites, user, comment, postedBy]
+export const schemaTypes = []
